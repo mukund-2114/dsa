6fd17e928cfe6f748b55a7d6a8c6b980dbb51813
Mukund Kapadia <mdrkkapadia@gmail.com>
2024-03-03 16:13:30 -0500

    bst insert search operation

---
+++ Data Structures/Binary Search Tree/bst.js
@@ -0,0 +1,79 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+
+    isEmpty(){
+        return this.root === null;
+    }
+
+    insert(value){
+        const newNode = new Node(value);
+        if(this.isEmpty()){
+            this.root = newNode;
+        }
+        else{
+            this.insertNode(this.root,newNode);
+        }
+    }
+    insertNode(root,newNode){
+        if(newNode.value < root.value){
+            if(root.left === null){
+                root.left = newNode
+            }
+            else{
+                this.insertNode(root.left,newNode);
+            }
+        }
+        else{
+            if(root.right === null){
+                root.right = newNode
+            }
+            else{
+                this.insertNode(root.right,newNode);
+            }
+        }
+    }
+
+    search(root, value){
+        if(!root){
+            return false
+        }
+        else{
+            if(root.value === value){
+                return true
+            }
+            else if(value < root.value){
+                return this.search(root.left, value)
+            }
+            else{
+                return this.search(root.right, value)
+            }
+        }
+    }
+
+}
+
+
+const bst = new BinarySearchTree();
+console.log("Tree is empty?" , bst.isEmpty());
+
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+
+// console.log(bst)
+
+console.log(bst.search(bst.root, 15))
+console.log(bst.search(bst.root, 3))
+console.log(bst.search(bst.root, 85))
